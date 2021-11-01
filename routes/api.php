c0bcaca346325bdfd343d34de2dabe74e8a63ae5
Bighsueh <40841121@gm.nfu.edu.tw>
2021-11-01 08:21:29 +0800

    add route /getMelody

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/{value?}', "MelodyController@getMelody");
+Route::get('/{value?}', "MelodyController@storeMelody");
+Route::get('/getMelody', "MelodyController@getMelody");
